c5318214ca0e730084c2886b1dcf2ccf11e2b65c
Wafaaa2006 <148413812+Wafaaa2006@users.noreply.github.com>
2023-10-23 22:34:10 +0700

    Add files via upload

---
+++ tugas percabangan 7 revisi.py
@@ -0,0 +1,18 @@
+#Nama: M. Najahul Wafa
+#Kelas: XI TKJ 1
+#Absen: 19
+#Soal: Sebagai seorang administrator sistem, Anda perlu memutuskan apakah suatu sistem perlu di-restart setelah pembaruan perangkat lunak.
+
+def cek_perlu_restart(pembaruan_perangkat_lunak):
+    if pembaruan_perangkat_lunak.lower() == "ya":
+        print("Sistem perlu di-restart.")
+    else:
+        print("Sistem tidak perlu di-restart.")
+
+def main():
+    pembaruan_perangkat_lunak = input("Apakah ada pembaruan perangkat lunak? (ya/tidak): ")
+
+    cek_perlu_restart(pembaruan_perangkat_lunak)
+
+if __name__ == "__main__":
+    main()
